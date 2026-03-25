6bc7202ba8f99092987feea4fb185b6b97ae7043
taehwann <taehwankim000@gmail.com>
2026-03-25 18:51:32 +0000

    Deploying to gh-pages from @ taehwann/taehwann.github.io@e6727fb43f8294661b8297b93b2d11d72dbda2d4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Projects I have worked on.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
